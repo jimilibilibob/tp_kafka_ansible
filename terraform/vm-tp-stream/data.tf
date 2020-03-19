8cfced4f2d24c41b96e1bdc1120a46e9c777e319
FONTAINE THOMAS <TFO17882@open-groupe.com>
2020-03-19 01:53:32 +0100

    ajout terraform

---
+++ terraform/vm-tp-stream/data.tf
@@ -0,0 +1,11 @@
+data "azurerm_shared_image_gallery" "sig_gallery" {
+  name                = var.sig_gallery_name
+  resource_group_name = var.sig_gallery_resource_group
+}
+
+data "azurerm_shared_image_version" "image_pki" {
+  name                = "1.0.0"
+  image_name          = "imd-tp-stream"
+  gallery_name        = data.azurerm_shared_image_gallery.sig_gallery.name
+  resource_group_name = data.azurerm_shared_image_gallery.sig_gallery.resource_group_name
+}
